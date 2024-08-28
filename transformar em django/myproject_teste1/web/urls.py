fc87ca69a53bf9d3f17d85e850d56e4e72cd901d
solucionai <compartilhadosolucionai@gmail.com>
2024-08-27 22:42:30 -0300

    Delete transformar em django directory

--- transformar em django/myproject_teste1/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<str:page>/', views.render_page, name='render_page'),
-    path('', views.render_page, {'page': 'index'}, name='home'),
-    path('process_form/', views.process_form, name='process_form'),
-]
